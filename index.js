efc22334464813a849a910e0233bccd312aef431
7J647 <007J647@gmail.com>
2020-09-23 00:52:27 -0400

    added and tested second prompt for description

--- index.js
+++ index.js
@@ -9,6 +9,42 @@ inquirer
       name: "What is the title of your project?"
     },
 
+    {
+        type: "input",
+        message: "description",
+        name: "What is the description of your project?",
+      },
+
+
+// // array of questions for user
+// const questions = [
+
+// ];
+
+// // function to write README file
+// function writeToFile(fileName, data) {
+// }
+
+// // function to initialize program
+// function init() {
+
+// }
+
+// // function call to initialize program
+// init();
+
+
+
+
+
+
+
+
+
+
+
+
+
   ])
   .then(function(response) {
       console.log(response);
