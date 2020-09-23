5fd9a8bc64ea041b9c3f36322f0162a6b32a3c56
7J647 <007J647@gmail.com>
2020-09-23 00:49:48 -0400

    set up inquirer.prompt and tested first question for title

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 var inquirer = require("inquirer");
 var fs = require("fs");
 
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "title",
+      name: "What is the title of your project?"
+    },
+
+  ])
+  .then(function(response) {
+      console.log(response);
+  })
+
+
+
 
