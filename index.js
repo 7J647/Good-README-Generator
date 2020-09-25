51c5dcf1bb5e15283a46e716cc600c7522d86bf3
7J647 <007J647@gmail.com>
2020-09-24 20:24:46 -0400

    cleaned up formatting for production

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 var inquirer = require("inquirer");
 var fs = require("fs");
 
-//importing from generateMarkdown.js
 var generateMarkdown = require("./generateMarkdown");
 
-//generating the command line questions
 inquirer
   .prompt([
     {
       type: "input",
       name: "title",
-      //this below is what appears in the prompt
       message: "What is the title of your project?",
     },
 
@@ -19,13 +16,6 @@ inquirer
       name: "description",
       message: "What is the description of your project?",
     },
-
-    // {
-    //   type: "checkbox",
-    //   name: "contents",
-    //   message: "Which sections will go in your table of contents?",
-    //   choices: ["Introduction", "URL", "Screenshots", "License", "Contact"],
-    // },
 
     {
       type: "input",
@@ -43,73 +33,39 @@ inquirer
       type: "list",
       name: "license",
       message: "Which license do you want to include?",
-      //these are the license choices
-      choices: ["MIT", "Apache", "License3", "License4"],
+      choices: ["MIT", "Apache", "Boost"],
     },
 
     {
-        type: "input",
-        name: "contributing",
-        message: "Who contributed to the development of this project?",
-      },
+      type: "input",
+      name: "contributing",
+      message: "Who contributed to the development of this project?",
+    },
       
-      {
-        type: "input",
-        name: "tests",
-        message: "What are the testing instructions for this project?",
-      },
+    {
+      type: "input",
+      name: "tests",
+      message: "What are the testing instructions for this project?",
+    },
 
-      {
-        type: "input",
-        name: "github",
-        message: "What is your Github username/handle?",
+    {
+      type: "input",
+      name: "github",
+      message: "What is your Github username/handle?",
     
-      },
+    },
 
-      {
-        type: "input",
-        name: "email",
-        message: "What is your email address?",
+    {
+      type: "input",
+      name: "email",
+      message: "What is your email address?",
     
-      },
+    },
         
-        
-    // // array of questions for user
-    // const questions = [
-
-    // ];
-
-    // // function to write README file
-    // function writeToFile(fileName, data) {
-    // }
-
-    // // function to initialize program
-    // function init() {
-
-    // }
-
-    // // function call to initialize program
-    // init();
   ])
   .then(function (response) {
     console.log(response);
-    //created variable to hold the response data
     const readMeText = generateMarkdown(response);
     console.log(readMeText);
-    //wrote the response data to the README file, used synch to avoid asynchronicity issues
     fs.writeFileSync("./output/README.MD", readMeText);
   });
-
-
-
-//   fs.writeFile("log.txt", process.argv[2], function(err) {
-
-//     if (err) {
-//       return console.log(err);
-//     }
-  
-//     console.log("Success!");
-  
-//   });
-  //need right format for data
-  //create the file
